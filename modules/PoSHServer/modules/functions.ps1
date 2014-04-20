da58716a6c2a27dad5351c5b2c44f436113242f4
Yusuf Ozturk <yusuf.ozturk@outlook.com>
2014-04-21 00:53:03 +0300

    PoSHServer v3.7
    New release - v3.7

--- modules/PoSHServer/modules/functions.ps1
+++ modules/PoSHServer/modules/functions.ps1
@@ -345,8 +345,22 @@ param (
 		.mpg {"video/mpeg"}
 		.mpeg {"video/mpeg"}
 		.mp3 {"audio/mpeg"}
+		.oga {"audio/ogg"}
+		.spx {"audio/ogg"}
+		.mp4 {"video/mp4"}
+		.m4v {"video/m4v"}
+		.ogg {"video/ogg"}
+		.ogv {"video/ogg"}
+		.webm {"video/webm"}
 		.wmv {"video/x-ms-wmv"}
 		.woff {"application/x-font-woff"}
+		.eot {"application/vnd.ms-fontobject"}
+		.svg {"image/svg+xml"}
+		.svgz {"image/svg+xml"}
+		.otf {"font/otf"}
+		.ttf {"application/x-font-ttf"}
+		.xht {"application/xhtml+xml"}
+		.xhtml {"application/xhtml+xml"}
 		default {"text/html"}
 	}	
 }
@@ -536,10 +550,10 @@ param (
 			{
 				$PostName = $PostName.Substring(0,$PostName.Length-2)
 
-				if (!$Properties."$PostName")
+				if (!(New-Object PSObject -Property @{PostName=@()}).PostName)
 				{
 					$Properties | Add-Member NoteProperty $Postname (@())
-					$Properties."$PostName" = $PostValue
+					$Properties."$PostName" += $PostValue
 				}
 				else
 				{
